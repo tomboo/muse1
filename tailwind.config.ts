c61916a3f74ef89d1766f8ef47406a5416a2b312
Tom Booster <tomboo.dev@gmail.com>
2025-09-26 07:31:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,5 +95,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
