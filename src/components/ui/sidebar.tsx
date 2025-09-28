64bc537b4a98b9a0801bb9de24f6672e14bac2f0
Tom Booster <tomboo.dev@gmail.com>
2025-09-28 00:49:13 +0000

    clicking on expand sidebar generation the following error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -82,7 +82,7 @@ const SidebarProvider = React.forwardRef<
     )
 
     // Helper to toggle the sidebar.
-    const toggleSidebar = React.useCallback(() => {
+    const toggleSidebar = React. useCallback(() => {
       return isMobile
         ? setOpenMobile((open) => !open)
         : setOpen((open) => !open)
